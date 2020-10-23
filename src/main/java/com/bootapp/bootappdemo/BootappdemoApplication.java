d7456a179c65450061dc0d99fa70d877be2b9b03
rahulvailapully <31269813+rahulvailapully@users.noreply.github.com>
2020-10-23 16:35:40 +0100

    First commit

---
+++ src/main/java/com/bootapp/bootappdemo/BootappdemoApplication.java
@@ -0,0 +1,13 @@
+package com.bootapp.bootappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootappdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootapp/bootappdemo/BootappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootapp.bootappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
